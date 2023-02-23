76bcf5b9866932d2aa83d5a56d214bfad34b65c9
Ali Yıldırım <95253154+ylrmali@users.noreply.github.com>
2023-02-23 10:42:21 +0300

    delete rest_framework library

--- movieapp/settings.py
+++ movieapp/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie.apps.MovieConfig',
-    'rest_framework',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
